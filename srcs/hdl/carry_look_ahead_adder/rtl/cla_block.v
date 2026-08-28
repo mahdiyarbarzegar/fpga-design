9d84945bfdf8d9b5d186b11e857d2972c83a1868
Mahdiyar <mahdiyar.barzegar@gmail.com>
2026-08-28 12:15:16 +0330

    format: modify code format

--- srcs/hdl/carry_look_ahead_adder/rtl/cla_block.v
+++ srcs/hdl/carry_look_ahead_adder/rtl/cla_block.v
@@ -1,33 +1,31 @@
-`timescale 1ns / 10ps
-
 module cla_block #(
-  parameter BUS_WIDTH = 4
+    parameter BUS_WIDTH = 4
 ) (
-  input                  cin,
-  input  [BUS_WIDTH-1:0] in1,
-  input  [BUS_WIDTH-1:0] in2,
-  output [BUS_WIDTH-1:0] s,
-  output                 cout
+    input                  cin,
+    input  [BUS_WIDTH-1:0] in1,
+    input  [BUS_WIDTH-1:0] in2,
+    output [BUS_WIDTH-1:0] s,
+    output                 cout
 );
 
-  wire [BUS_WIDTH-1:0] p;
-  wire [BUS_WIDTH-1:0] g;
-  wire [  BUS_WIDTH:0] c;
+    wire [BUS_WIDTH-1:0] p;
+    wire [BUS_WIDTH-1:0] g;
+    wire [  BUS_WIDTH:0] c;
 
-  assign c[0] = cin;
-  assign p    = in1 ^ in2;
-  assign g    = in1 & in2;
+    assign c[0] = cin;
+    assign p    = in1 ^ in2;
+    assign g    = in1 & in2;
 
-  assign s    = c[BUS_WIDTH-1:0] ^ p;
-  assign cout = c[BUS_WIDTH];
+    assign s    = c[BUS_WIDTH-1:0] ^ p;
+    assign cout = c[BUS_WIDTH];
 
-  carry_predictor #(
-    .CARRY_WIDTH(BUS_WIDTH)
-  ) cp (
-    .cin (cin),
-    .p   (p),
-    .g   (g),
-    .cout(c[BUS_WIDTH:1])
-  );
+    carry_predictor #(
+        .CARRY_WIDTH(BUS_WIDTH)
+    ) cp (
+        .cin (cin),
+        .p   (p),
+        .g   (g),
+        .cout(c[BUS_WIDTH:1])
+    );
 
 endmodule
